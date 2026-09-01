15f11a7a146a2f6b4ed7136b46c34760c93ff048
Nathan Chancellor <nathan@kernel.org>
2026-08-31 21:49:49 -0700

    fixup! runner: Add files for generating a build environment container
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- runner/push_container_image.sh
+++ runner/push_container_image.sh
@@ -17,8 +17,15 @@ fi
 
 if ! skopeo login --get-login ghcr.io &>/dev/null; then
     echo '[+] Logging into ghcr.io'
-    skopeo login ghcr.io
+    if [ -n "${GITHUB_ACTIONS:-}" ]; then
+        skopeo_args=(-u "$GITHUB_ACTOR" -p "$GITHUB_TOKEN")
+    else
+        skopeo_args=()
+    fi
+    skopeo login "${skopeo_args[@]}" ghcr.io
 fi
 
 echo "[+] Uploading ${output##*/} to $ghcr_namespace"
-skopeo copy oci:"$output" docker://"$ghcr_namespace"/"${output##*/}":latest
+ghcr_image=docker://"$ghcr_namespace"/"${output##*/}"
+skopeo copy oci:"$output" "$ghcr_image":latest
+skopeo copy "$ghcr_image":latest "$ghcr_image":"$(date +'%Y-%m-%d-%H-%M')"
