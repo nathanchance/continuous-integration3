0cd32ee18145cd18ceeae39b787d7b1a93536d48
Nathan Chancellor <nathan@kernel.org>
2026-09-01 16:01:44 -0700

    fixup! runner: Add basic driver
    Rewrite driver in a more modular fashion with various small fixes along
    the way
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- runner/driver.py
+++ runner/driver.py
@@ -10,7 +10,7 @@
 import os
 import subprocess
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from pathlib import Path
 
 import requests
@@ -18,104 +18,170 @@ import tuxmake.build
 
 MIRROR_GIT = 'git://192.168.122.2'
 MIRROR_HTTP = f"{MIRROR_GIT.replace('git', 'http')}:8080"
+VALID_LLVM_VERS = tuple(range(23, 21, -1))
+VALID_TREES = ('linux',)
 
 
-def print_git_checkout_info(git_repo: Path) -> None:
-    info_cmd = ['git', '-C', git_repo, 'show', '-s', '--format=%h ("%s", %cs)']
+def clone_mirror_repo(remote_repo_name: str, local_repo_name: str = '') -> Path:
+    if not local_repo_name:
+        local_repo_name = remote_repo_name
+
+    local_repo = Path('/', local_repo_name)
+    remote_repo = f"{MIRROR_GIT}/{remote_repo_name}.git"
+
+    print(f"[+] Cloning {remote_repo} to {local_repo}")
+    subprocess.run(['git', 'clone', '--depth=1', '--quiet', remote_repo, local_repo], check=True)
+
+    info_cmd = ['git', '-C', local_repo, 'show', '-s', '--format=%h ("%s", %cs)']
     info_output = subprocess.run(
         info_cmd, capture_output=True, check=True, text=True
     ).stdout.strip()
-    branch_cmd = ['git', '-C', git_repo, 'rev-parse', '--abbrev-ref', 'HEAD']
+    branch_cmd = ['git', '-C', local_repo, 'rev-parse', '--abbrev-ref', 'HEAD']
     branch_output = subprocess.run(
         branch_cmd, capture_output=True, check=True, text=True
     ).stdout.strip()
-    print(f"[+] Successfully checked out {git_repo.name} at {branch_output}: {info_output}")
+    print(f"[+] Successfully checked out {local_repo.name} at {branch_output}: {info_output}")
+
+    return local_repo
 
 
-def main():
-    parser = ArgumentParser(description='Build and boot driver')
+def parse_arguments():
+    parser = ArgumentParser(
+        description='Build and boot driver', formatter_class=ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument('-a', '--arch', required=True, help='Architecture to build')
     parser.add_argument('-b', '--boot', action='store_true', help='Boot kernel after build')
     parser.add_argument(
         '-k', '--kconfigs', required=True, nargs='+', help='Kconfig values for tuxmake'
     )
     parser.add_argument(
+        '-l',
+        '--llvm-version',
+        choices=VALID_LLVM_VERS,
+        default=VALID_LLVM_VERS[0],
+        type=int,
+        help='LLVM version to build with',
+    )
+    parser.add_argument(
+        '-t', '--tree', choices=VALID_TREES, default=VALID_TREES[0], help='Tree to build'
+    )
+    parser.add_argument(
         '-v', '--verbose', action='store_true', help='Perform verbose build in tuxmake'
     )
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    llvm_ver = '23'
-    kernel_tree = 'linux'
 
-    result = requests.get(f"{MIRROR_HTTP}/toolchains/latest_llvm_releases.json", timeout=15)
-    result.raise_for_status()
-    llvm_releases = result.json()
+class Runner:
+    def __init__(self) -> None:
+        self.arch: str = ''
+        self.boot: bool = False
+        self.kconfigs: list[str] = []
+        self.llvm_version: int = 0
+        self.tree: str = ''
+        self.verbose: bool = False
 
-    if not (toolchain_tarball := llvm_releases.get(llvm_ver)):
-        msg = f"LLVM {llvm_ver} requested but not in latest_llvm_releases.json?"
-        raise RuntimeError(msg)
+        self._make_vars: dict[str, str] = {'LLVM': '1', 'LLVM_IAS': '1'}
+        self._toolchain_prefix: Path = Path()
+        self._source_path: Path = Path()
+        self._boot_utils_path: Path = Path()
+        self._output_path: Path = Path('/output')
 
-    if not (toolchain := Path('/', toolchain_tarball.replace('.tar.xz', ''))).exists():
+    def _prepare_toolchain(self) -> None:
+        # Fetch latest available toolchains from mirror VM
+        result = requests.get(f"{MIRROR_HTTP}/toolchains/latest_llvm_releases.json", timeout=15)
+        result.raise_for_status()
+        if not (toolchain_tarball := result.json().get(str(self.llvm_version))):
+            msg = f"LLVM {self.llvm_version} requested but not in latest_llvm_releases.json?"
+            raise RuntimeError(msg)
+
+        # Download and extract toolchain into build container
+        self._toolchain_prefix = Path('/', toolchain_tarball.replace('.tar.xz', ''))
         tar_url = f"{MIRROR_HTTP}/toolchains/{toolchain_tarball}"
         print(f"[+] Downloading {tar_url}")
-        result = requests.get(tar_url, timeout=10)
+        result = requests.get(tar_url, timeout=15)
         result.raise_for_status()
-        print(f"[+] Extracting {toolchain_tarball} to {toolchain}")
+        print(f"[+] Extracting {toolchain_tarball} to {self._toolchain_prefix}")
         subprocess.run(
-            ['tar', '-C', toolchain.parent, '-f', '-', '-J', '-x'], check=True, input=result.content
+            ['tar', '-C', self._toolchain_prefix.parent, '-f', '-', '-J', '-x'],
+            check=True,
+            input=result.content,
         )
 
-    base_git_clone_cmd = ['git', 'clone', '--depth=1', '--quiet']
+    def _prepare_git(self) -> None:
+        self._source_path = clone_mirror_repo(self.tree)
+        if self.boot:
+            self._boot_utils_path = clone_mirror_repo('boot-utils')
 
-    if not (source := Path('/', kernel_tree)).exists():
-        git_url = f"{MIRROR_GIT}/{kernel_tree}.git"
-        print(f"[+] Cloning {git_url} to {source}")
-        subprocess.run([*base_git_clone_cmd, git_url, source], check=True)
-    print_git_checkout_info(source)
+    def _build(self) -> None:
+        # It would be nicer to use LLVM=<prefix>/bin/ here but tuxmake ensures
+        # the compiler is in PATH
+        os.environ['PATH'] = f"{self._toolchain_prefix}/bin:{os.environ['PATH']}"
 
-    if args.boot and not (boot_utils := Path('/boot-utils')).exists():
-        git_url = f"{MIRROR_GIT}/boot-utils.git"
-        print(f"[+] Cloning {git_url} to {boot_utils}")
-        subprocess.run([*base_git_clone_cmd, git_url, boot_utils], check=True)
-        print_git_checkout_info(boot_utils)
+        print('[+] Calling tuxmake to build kernel')
+        result = tuxmake.build.build(
+            build_dir=Path('/build'),
+            kconfig=self.kconfigs[0],
+            kconfig_add=self.kconfigs[1:],
+            make_variables=self._make_vars,
+            output_dir=self._output_path,
+            target_arch=self.arch,
+            targets=['kernel' if self.boot else 'default'],
+            toolchain='clang',
+            tree=self._source_path,
+            verbose=self.verbose,
+        )
+        if result.failed:
+            sys.exit(1)
 
-    print('[+] Calling tuxmake to build kernel')
-    os.environ['PATH'] = f"{toolchain}/bin:{os.environ['PATH']}"
-    output = Path('/output')
-    result = tuxmake.build.build(
-        build_dir=Path('/build'),
-        kconfig=args.kconfigs[0],
-        kconfig_add=args.kconfigs[1:],
-        make_variables={'LLVM': '1', 'LLVM_IAS': '1'},
-        output_dir=output,
-        target_arch=args.arch,
-        targets=['kernel' if args.boot else 'default'],
-        toolchain='clang',
-        tree=source,
-        verbose=args.verbose,
-    )
-    if result.failed:
-        sys.exit(1)
+    def _boot(self) -> None:
+        if not self.boot:
+            return
 
-    if args.boot:
         gh_releases_json_url = f"{MIRROR_HTTP}/boot-utils/releases.json"
         gh_releases_json = Path(gh_releases_json_url.replace(MIRROR_HTTP, ''))
         (result := requests.get(gh_releases_json_url, timeout=15)).raise_for_status()
         gh_releases_json.write_bytes(result.content)
 
-        boot_qemu_py = Path(boot_utils, 'boot-qemu.py')
+        boot_qemu_py = Path(self._boot_utils_path, 'boot-qemu.py')
         print(f"[+] Running {boot_qemu_py.name}")
         boot_qemu_py_cmd = [
             boot_qemu_py,
             '-a',
-            args.arch,
+            self.arch,
             '--gh-json-file',
             gh_releases_json,
             '-k',
-            output,
+            self._output_path,
         ]
         print(f"$ {' '.join(str(x) for x in boot_qemu_py_cmd)}")
         subprocess.run(boot_qemu_py_cmd, check=True)
+
+    def run(self) -> None:
+        # download toolchain
+        self._prepare_toolchain()
+
+        # clone git repositories
+        self._prepare_git()
+
+        # build kernel
+        self._build()
+
+        # boot kernel if requested
+        self._boot()
+
+
+def main() -> None:
+    args = parse_arguments()
+
+    runner = Runner()
+    runner.arch = args.arch
+    runner.boot = args.boot
+    runner.kconfigs = args.kconfigs
+    runner.llvm_version = args.llvm_version
+    runner.tree = args.tree
+    runner.verbose = args.verbose
+
+    runner.run()
 
 
 if __name__ == '__main__':
