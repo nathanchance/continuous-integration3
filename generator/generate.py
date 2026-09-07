ac30555cf4fca5aa19d8d400b34544e1f7c21878
Nathan Chancellor <nathan@kernel.org>
2026-09-06 18:30:39 -0700

    fixup! generator: Initial driver
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- generator/generate.py
+++ generator/generate.py
@@ -20,11 +20,13 @@ class Workflow:
     PODMAN_RUN_DRIVER = (
         'podman', 'run',
         '--env', 'GITHUB_ACTIONS',
+        '--env', 'GITHUB_OUTPUT',
         '--env', 'GITHUB_WORKSPACE',
         '--pull', 'newer',
         '--quiet',
         '--rm',
         '--tty',
+        '--volume', '$GITHUB_OUTPUT:$GITHUB_OUTPUT',
         '--volume', '$GITHUB_WORKSPACE:/work:ro',
         'ghcr.io/nathanchance/ci3-kernel-build-env:latest',
         '/work/runner/driver.py',
@@ -54,7 +56,7 @@ class Workflow:
                     {
                         'name': f"Generate pinned revision for {self.tree}",
                         'id': 'genrev',
-                        'run': f"echo \"revision=$({' '.join(self.PODMAN_RUN_DRIVER)} gen-revision {self.tree})\" >>\"$GITHUB_OUTPUT\"",
+                        'run': f"{' '.join(self.PODMAN_RUN_DRIVER)} gen-revision {self.tree}",
                     },
                 ],
             },
