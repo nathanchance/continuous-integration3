300c8a2302f9c93934a70e73ad854234686b5154
Nathan Chancellor <nathan@kernel.org>
2026-09-06 22:59:56 -0700

    fixup! infrastructure: Add files for building virtual machine images with mkosii
    Clone llvm-project.git and tc-build.git to be able to build LLVM from
    main
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- infra/vm/mkosi.profiles/mirror/mkosi.extra/opt/mirror.py
+++ infra/vm/mkosi.profiles/mirror/mkosi.extra/opt/mirror.py
@@ -60,6 +60,8 @@ def setup_srv_git() -> None:
 
     repo_urls = [
         'https://github.com/ClangBuiltLinux/boot-utils.git',
+        'https://github.com/ClangBuiltLinux/tc-build.git',
+        'https://github.com/llvm-project/llvm-project.git',
     ]
     for repo_url in repo_urls:
         if (repo := Path(GIT_DIR, Path(repo_url).name)).exists():
